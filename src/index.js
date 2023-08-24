49c68e6a053006d710756b222d8dcd31b7de165a
Kamran Aslam <kamranaslam184@gmail.com>
2023-08-24 16:34:48 +0500

    new chnages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ContextProvider } from "./context/Context.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ContextProvider>
   </React.StrictMode>
 );
 
